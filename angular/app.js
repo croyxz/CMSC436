011eb5a991ad9a8d43bdc1a3c92d3abc711f3420
Erik Koebke <ekoebke@terpmail.umd.edu>
2014-02-14 17:49:45 -0500

    updated

--- angular/app.js
+++ angular/app.js
@@ -1,14 +1,3 @@
 var pushNotify = angular.module('pushNotify', []);
 
-pushNotify.controller('submitInfo', function ($scope, $http) {
-    $scope.formData = {};
 
-    $scope.sendForm = function () {
-        /*$http.post('*', $scope.formData);*/
-
-        /*$http.post('*', {"recipient": $scope.formData.recipient, "badge": $scope.formData.badge,
-            "key": $scope.formData.key, "field": $scope.formData.field});*/
-
-        console.log("submitInfo");
-    }
-});
--- angular/controllers.js
+++ angular/controllers.js
@@ -1,12 +1,39 @@
-// pushNotify.controller('pushCtrl',['$scope', function (scope_var) {
-//     scope_var.fields = [{id: 'field1'}];
-// 
-//     scope_var.addNewField = function () {
-//         var newFieldNum = $scope.fields.length + 1;
-//         scope_var.fields.push({'id':'field'+newFieldNum});
-//     }
-// 
-//     scope_var.showAddButton = function (field) {
-//         return field.id = scope_var.fields[scope_var.fields.length - 1].id;
-//     }
-// }]);
+//to finish tomorrow
+pushNotify.controller('InfoTools', function ($scope, $http) {
+
+    localStorage.setItem("erik", "koebke");
+
+    $scope.formData = {
+
+    };
+
+    $scope.returnData = {
+
+    };
+
+    $scope.postForm = function () {
+        $http.post('*', {"recipient": $scope.formData.recipient, "badge": $scope.formData.badge,
+            "k": $scope.formData.k, "field": $scope.formData.field}).success(function (data) {
+                console.log("OK", data)
+            }).error(function (err) {
+                "ERR", console.log(err)
+            });
+    }
+
+    $scope.getForm = function () {
+        $http.get('http://echo.jsontest.com/erik/koebke/ryo/hang').then(function(response) {
+            $scope.returnData = response.data;
+        })
+    }
+
+    $scope.putForm = function () {
+        $http.put('', {"recipient": $scope.formData.recipient, "badge": $scope.formData.badge,
+            "key": $scope.formData.key, "field": $scope.formData.field}).success(function (data) {
+                console.log("OK", data)
+            }).error(function (err) {
+                "ERR", console.log(err)
+            });
+    }
+
+});
+
